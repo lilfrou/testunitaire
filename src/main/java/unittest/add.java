b923f8ba9008b96f5adb397a9db1c3e394c7e70b
firas <firas@gmail.com>
2019-08-14 20:24:45 +0100

    first commit

---
+++ src/main/java/unittest/add.java
@@ -0,0 +1,10 @@
+package unittest;
+
+public class add {
+	
+		  public static int additioner(int a, int b) {
+		    return a + b;
+		  }
+		}
+
+
---
+++ src/test/java/unittest/addtest.java
@@ -0,0 +1,14 @@
+package unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class addtest {
+
+	@Test
+	 public void testAdditioner() {
+        assertTrue(add.additioner(2,2) == 4);           
+  }
+
+}
